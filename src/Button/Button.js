c8114f6b87309248e3fdd799e2e86fadc329f0c9
Sokrat Poghosyan <sorkat.poghosyan@gmail.com>
2021-01-15 19:50:26 +0400

    Fixed comment

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Button = () => {
+const Button = ({ onCLick }) => {
     return (
-        <button>
+        <button onClick={onCLick}>
             Click
         </button>
     )
