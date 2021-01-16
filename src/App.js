714fe2c6dfe2abd42ca8fa9797514d043632fd15
Armine Avdalyan <armineavdalyan85@gmail.com>
2021-01-17 00:06:19 +0400

    icon test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import Icon from './Icon/Icon-ShoppingCart';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-        
+        <Icon />
     </div>
   );
 }
