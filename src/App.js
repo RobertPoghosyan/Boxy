cc568fc71cce45cfb1b2e833e84c6a3e207a611d
Armine Avdalyan <armineavdalyan85@gmail.com>
2021-01-17 00:21:49 +0400

    iconBoxy test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import Icon from './Icon/Icon-ShoppingCart';
-
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
-        <Icon />
+        
     </div>
   );
 }
