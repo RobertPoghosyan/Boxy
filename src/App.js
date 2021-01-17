f2e8982d73a4c56d4bd76fcc5ed8d74f44868477
TatevAsatryan96 <tatev.asatryan1996@gmail.com>
2021-01-17 18:38:55 +0400

    Create header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import Button from './components/Button/Button';
-import Header from './containers/Header/header';
+import Header from "./containers/Header/header";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-       <Button className="btn-header" > Մուտք </Button> 
-       <Button className="btn-main" > Ստանալ </Button> 
-       <Button className="btn-footer" > Առաջարկ ու՞նես </Button> 
     </div>
   );
 }
--- src/containers/Header/header.js
+++ src/containers/Header/header.js
@@ -1,30 +1,40 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-import Button from '../../components/Button/Button'
-import './header.scss'
+import Button from "../../components/Button/Button";
+import BoxyIcon from "../../icons/Boxy/BoxyIcon";
+import ShoppingCart from "../../icons/ShoppingCart/ShoppingCart";
+import "./header.scss";
 
 function Header() {
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const listenScrollEvent = (e) => {
-        if (window.scrollY > 100) {
-            setIsScrolled(true);
-        } else {
-            setIsScrolled(false);
-        }
+  const listenScrollEvent = (e) => {
+    if (window.scrollY > 100) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
     }
-    
-    const componentDidMount = () => {
-        window.addEventListener('scroll', listenScrollEvent)
-    }
+  };
 
-    return (
-        <div>
-            <header id='app-header' className={isScrolled ? 'app-header--scrolled' : 'app-header'}>
-                    <Button className="btn-header" > Մուտք </Button> 
-            </header>
-            
+  const componentDidMount = () => {
+    window.addEventListener("scroll", listenScrollEvent);
+  };
+
+  return (
+    <div>
+      <header
+        id="app-header"
+        className={isScrolled ? "app-header--scrolled" : "app-header"}
+      >
+        <div className="BoxIcon">
+          <BoxyIcon />
         </div>
-    )
+        <div className="ShoppingCart">
+          <ShoppingCart />
+        </div>
+        <Button className="btn-header"> Մուտք </Button>
+      </header>
+    </div>
+  );
 }
 export default Header;
