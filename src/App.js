b33a63bc189e13262ba505f96ecfde9c3ccecbec
TatevAsatryan96 <tatev.asatryan1996@gmail.com>
2021-01-17 17:02:20 +0400

    Create header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import Button from './components/Button/Button'
+import Button from './components/Button/Button';
+import Header from './containers/Header/header';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
        <Button className="btn-header" > Մուտք </Button> 
        <Button className="btn-main" > Ստանալ </Button> 
        <Button className="btn-footer" > Առաջարկ ու՞նես </Button> 
