f40eeb481e23b9a36b7bad02e3bc78f826232ee3
RobertPoghosyan <[robertpoghosian@gmail.com]>
2021-01-28 16:41:42 +0400

    Cleared code

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -3,8 +3,6 @@ import React from "react";
 import BottomFooter from "../../components/BottomFooter/BottomFooter";
 import TopFooter from "../../components/TopFooter/TopFooter";
 
-import "./Footer.scss";
-
 const Footer = () => {
   return (
     <div className="app-footer">
